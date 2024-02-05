8b87be5d5312f2a6b337354df2e5ed10ff3ff546
adithya-yadav <82747047+adithya-yadav@users.noreply.github.com>
2024-02-05 12:01:38 +0530

    Error alert added

--- src/customeHook/useApiData.jsx
+++ src/customeHook/useApiData.jsx
@@ -6,7 +6,7 @@ function useApiData() {
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/all").then((res) => {
             setApiData(res.data.slice(0, 10))
-        })
+        }).catch((e)=>alert(e.message?e.message:"something went wrong"))
     }, [])
   return {apiData}
 }
